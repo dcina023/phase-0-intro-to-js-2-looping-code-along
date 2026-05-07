2a0be3b9a2dfc16f1ac6e1d2f877d92f288f037b
+ <+>
2026-05-06 21:36:09 -0400

    complete looping lab

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
- function writeCards(name, event) {
- let messages = [];
-  for (let i =0; i < name.length; i++) {
-messages.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+function writeCards() {
+  const names = ["Guadalupe", "Ollie", "Aki"];
+   return names.map(name => `Thank you, ${name}, for the wonderful surprise gift!`);
+} // yay arrow functions. Played around with writing this in a couple different ways, originally I did a for loop and today I discovered map() 
+ // learned how it creates a new array by applying a function to every element of an existing array. 
+
+function countDown(loopsForDays) {
+  while (loopsForDays >= 0) {
+    console.log(loopsForDays);
+    loopsForDays--;
   }
-  
-  return messages;
-}
-function countDown(num) {
-    while (num >= 0) {
-    console.log(num);
-    num--;
-    }
 }
